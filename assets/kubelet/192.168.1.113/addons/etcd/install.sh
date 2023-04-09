bf7adc3c95e87b3a4398166679fd8a6e8a29c494
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2023-04-09 07:27:34 +0000

    Add poissonnerie

---
+++ assets/kubelet/192.168.1.113/addons/etcd/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+export LC_ALL=C
+ROOT=$(dirname "${BASH_SOURCE}")
+
+mkdir -p /etc/etcd/pki
+cp ${ROOT}/etcd.crt /etc/etcd/pki/
+cp ${ROOT}/etcd.key /etc/etcd/pki/
+cp ${ROOT}/ca.crt /etc/etcd/pki/
+cp ${ROOT}/etcd.env /etc/etcd/
+mkdir -p /etc/kubernetes/manifests
+cp ${ROOT}/etcd-server.yaml /etc/kubernetes/manifests/
