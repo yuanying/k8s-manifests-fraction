0bde20064e7efbe4509abe5bb818c869e4a8a934
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2024-05-18 04:41:37 +0000

    Bump k8s to v1.26

--- assets/kubelet-default/install-deps.sh
+++ assets/kubelet-default/install-deps.sh
@@ -13,7 +13,7 @@ CNI_VERSION="v1.2.0"
 mkdir -p /opt/cni/bin
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-${architecture}-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
 
-RELEASE="v1.25.15"
+RELEASE="v1.26.15"
 
 mkdir -p /opt/bin
 
@@ -40,7 +40,7 @@ rm -f /opt/bin/etcdctl
 mv ${ETCD_TMP}/etcdctl /opt/bin/etcdctl-${ETCD_VER}
 ln -s /opt/bin/etcdctl-${ETCD_VER} /opt/bin/etcdctl
 
-CRICTL_VER="v1.25.0"
+CRICTL_VER="v1.26.0"
 CRICTL_URL=https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VER}/crictl-${CRICTL_VER}-linux-${architecture}.tar.gz
 CRICTL_TMP=$(mktemp -d)
 curl -L ${CRICTL_URL} -o ${CRICTL_TMP}/crictl.tar.gz
